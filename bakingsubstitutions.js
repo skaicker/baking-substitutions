deb87b4519b1ed30d6566bc51577a07919356a20
skaicker <sanjana.kaicker@gmail.com>
2017-08-16 11:42:28 -0400

    Update bakingsubstitutions.js

--- bakingsubstitutions.js
+++ bakingsubstitutions.js
@@ -1,17 +1,22 @@
 function calculate(){
-  var x, text;
-  x = document.getElementById("word").value;
-  if (x == "butter") {
-    text = "One sec...";
+  var input, text;
+  input = document.getElementById("word").value;
+  if (input == "butter" || input == "Butter" || input == "BUTTER") {
+      text = "Oil, applesauce, or pumpkin puree is the way to go!"
+
   }
-  /*} else if (x == "sugar") {
-    text = "...";
-  } else if (x == "cornstarch") {
-    text = "...";
-  } else if (x == "oil") {
-    text = "...";
-  } else {
-    text = "Sorry!";
-  }*/
+    else if (input == "sugar" || input == "Sugar" || input =="SUGAR") {
+      text = "You can replace sugar with honey!)";
+  }
+    else if (input == "cornstarch" || input == "Cornstarch" || input == "CORNSTARCH"){
+       text = "You can substitute flour, but it might not come out well in all cases.";
+  }
+    else if (input == "oil" || input == "Oil" || input == "OIL") {
+      text = "Substitute butter or pumpkin puree!";
+  }
+    else {
+       text = "Sorry, we don't have information for that ingredient yet!";
+     }
+numInput = document.getElementById("num").value
 document.getElementById("click").innerHTML = text;
 }
