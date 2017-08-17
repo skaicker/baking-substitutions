b6213ba0f8aab79e7db0ec55218cb75e67744152
skaicker <sanjana.kaicker@gmail.com>
2017-08-17 10:51:23 -0400

    Update bakingsubstitutions.js

--- bakingsubstitutions.js
+++ bakingsubstitutions.js
@@ -4,10 +4,13 @@ function calculate(){
   text = document.getElementById("word").value;
   numInput = document.getElementById("num").value
   if (0 < parseInt(numInput) && input ==  "butter" || input == "Butter" || input == "BUTTER") {
-    text = numInput*3/4 + " tbsp oil";
+    text = numInput*3/4 + " tbsp. oil.";
   }
   else if (0 < parseInt(numInput) && input == "oil" || input == "Oil" || input == "OIL") {
-    text = numInput*4/3 + " tbsp butter";
+    text = numInput*4/3 + " tbsp. butter.";
+  }
+  else if (0 < parseInt(numInput) && input == "cornstarch" || input == "Cornstarch" || input == "CORNSTARCH") {
+    text = numInput*3 + " tbsp. flour. However, be careful, as this substitution may not work in all cases.";
   }
   else {
     text = "Sorry, we don't have substitutions for that ingredient yet!";
