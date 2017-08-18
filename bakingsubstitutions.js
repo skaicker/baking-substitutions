5fbc41976ed7dfdec53177beca4c2eb0de453f92
skaicker <sanjana.kaicker@gmail.com>
2017-08-18 13:37:49 -0400

    Update bakingsubstitutions.js

--- bakingsubstitutions.js
+++ bakingsubstitutions.js
@@ -3,29 +3,29 @@ function calculate(){
   input = document.getElementById("word").value;
   text = document.getElementById("word").value;
   numInput = document.getElementById("num").value
-  if (0 < parseInt(numInput) && input ==  "butter" || input == "Butter" || input == "BUTTER") {
+  if (0 < parseFloat(numInput) && input ==  "butter" || input == "Butter" || input == "BUTTER") {
     text = numInput*3/4 + " tbsp. oil or pumpkin puree.";
   }
-  else if (0 < parseInt(numInput) && input == "oil" || input == "Oil" || input == "OIL") {
+  else if (0 < parseFloat(numInput) && input == "oil" || input == "Oil" || input == "OIL") {
     text = numInput*4/3 + " tbsp. butter " + "OR " + numInput + " tbsp. pumpkin puree.";
   }
-  else if (0 < parseInt(numInput) && input == "cornstarch" || input == "Cornstarch" || input == "CORNSTARCH") {
+  else if (0 < parseFloat(numInput) && input == "cornstarch" || input == "Cornstarch" || input == "CORNSTARCH") {
     text = numInput*3 + " tbsp. flour. However, be careful, as this substitution may not work in all cases." + " You can also use " + numInput + " tbsp. arrowroot, or any other pure starch you have.";
   }
-  else if (0 < parseInt(numInput) && input == "baking soda" || input == "Baking Soda" || input == "BAKING SODA" || input == "Baking soda") {
+  else if (0 < parseFloat(numInput) && input == "baking soda" || input == "Baking Soda" || input == "BAKING SODA" || input == "Baking soda") {
     text = numInput*4 + " tbsp. baking powder and " + numInput + " tbsp. mild vinegar (which is meant to give some acidity).";
   }
-  else if (0 < parseInt(numInput) && input == "baking powder" || input == "Baking Powder" || input == "BAKING POWDER" || input == "Baking powder") {
+  else if (0 < parseFloat(numInput) && input == "baking powder" || input == "Baking Powder" || input == "BAKING POWDER" || input == "Baking powder") {
     text = numInput*1/4 + " tbsp. baking soda.";
   }
-  else if (0 < parseInt(numInput) && input ==  "vanilla"|| input == "Vanilla" || input == "VANILLA" || input== "vanilla extract" || input == "Vanilla Extract" || input == "VANILLA EXTRACT" || input == "Vanilla extract") {
+  else if (0 < parseFloat(numInput) && input ==  "vanilla"|| input == "Vanilla" || input == "VANILLA" || input== "vanilla extract" || input == "Vanilla Extract" || input == "VANILLA EXTRACT" || input == "Vanilla extract") {
     text = numInput + " tbsp. maple syrup (note: 1 tbsp = 3 tsp).";
   }
-  else if (0 < parseInt(numInput) && input == "cocoa powder" || input == "Cocoa Powder" || input == "COCOA POWDER" || input == "Cocoa powder" || input == "cocoa" || input == "COCOA" || input == "Cocoa") {
+  else if (0 < parseFloat(numInput) && input == "cocoa powder" || input == "Cocoa Powder" || input == "COCOA POWDER" || input == "Cocoa powder" || input == "cocoa" || input == "COCOA" || input == "Cocoa") {
     text = numInput*2/3 + " tbsp. chocolate and reduce fat in the recipe by " + numInput*1/3 + " tbsp.";
   }
-  else if (0 >= parseInt(numInput)) {
-    text = "Please enter in a whole value greater than 0!"
+  else if (0 >= parseFloat(numInput)) {
+    text = "Please enter in a value greater than 0!";
   }
   else {
     text = "Sorry, we don't have substitutions for that ingredient yet!";
