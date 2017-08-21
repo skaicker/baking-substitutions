0ac1fd3ae92175e191eb1cfd1363cfbcce4e5125
skaicker <sanjana.kaicker@gmail.com>
2017-08-21 15:58:18 -0400

    Update bakingsubstitutions.js

--- bakingsubstitutions.js
+++ bakingsubstitutions.js
@@ -24,11 +24,14 @@ function calculate(){
   else if (0 < parseFloat(numInput) && input == "cocoa powder" || input == "Cocoa Powder" || input == "COCOA POWDER" || input == "Cocoa powder" || input == "cocoa" || input == "COCOA" || input == "Cocoa") {
     text = numInput*2/3 + " tbsp. chocolate and reduce fat in the recipe by " + numInput*1/3 + " tbsp.";
   }
-  else if (0 < parseFloat(numInput) && input == "chocolate" || input == "Chocolate" || input == "CHOCOLATE") {
+  else if (0 < parseFloat(numInput) && input == "chocolate" || input == "Chocolate" || input == "CHOCOLATE"){
     text = numInput*3/2 + " tbsp. cocoa powder and add fat in the recipe by " + numInput*3 + " tbsp. However, be careful, since the sugar and fat content vary in different chocolate bars.";
   }
   else if (0 < parseFloat(numInput) && input == "milk" || input == "Milk" || input == "MILK"){
-    text = numInput + " tbsp. almond, soy, coconut, or rice milk or yogurt OR " + numInput*1/2 + " tbsp. evaporated or condensed milk."
+    text = numInput + " tbsp. almond, soy, coconut, or rice milk or yogurt OR " + numInput*1/2 + " tbsp. evaporated or condensed milk.";
+  }
+  else if (0 < parseFloat(numInput) && input == "cake flour" || input == "Cake Flour" || input == "Cake flour" || input == "CAKE FLOUR"){
+    text = numInput + " tbsp. flour, minus " + numInput*1/8 + " flour; add back in " + numInput*1/8 + " tbsp. cornstarch.";
   }
   else if (isNaN(numInput) || 0 >= parseFloat(numInput)) {
     text = "Please enter in a number value greater than 0!";
